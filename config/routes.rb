bb3b5b1a05edbeccc167ad2017baed2df8d8ea89
Simone Battiste-Alleyne <softe1988@gmail.com>
2014-12-19 15:08:30 -0500

    Static pages generated

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   
+  get 'static_pages/home'
+
+  get 'static_pages/about'
+
   # setting home page to Cuisines Index page
-  root to: 'cuisines#index'
+
 
   
   resources :cooking_styles
